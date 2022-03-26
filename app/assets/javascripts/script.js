a4196c154481b3fd5f070fe551687e130439947c
mimika74 <88548032+mimika74@users.noreply.github.com>
2022-03-26 12:23:34 +0000

    [add]layouts

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,7 +1,10 @@
 $(function() {
-  $('.menu-trigger').on('click', function(event) {
+  //$('.menu-trigger').on('click', function(event) {
+  $(document).on('click','.menu-trigger',function(event){
+
     $(this).toggleClass('active');
     $('#sp-menu').fadeToggle();
-    event.preventDefault();
+    //event.preventDefault();
+    return false;
   });
 });
