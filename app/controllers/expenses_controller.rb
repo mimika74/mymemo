4f542f215722dfe4cc85e9d5301abd951cf8c89f
mimika74 <88548032+mimika74@users.noreply.github.com>
2022-03-27 15:52:48 +0000

    [add]aggregate

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -115,10 +115,10 @@ class ExpensesController < ApplicationController
   def aggregate
     #@expense = Expense.select("date").group("date").sum("expense")
 
-    @thisWeekSum =  Expense.where(date: (Date.today - (Date.today.wday))..Date.today).sum(:expense)
-    @lastWeekSum =  Expense.where(date: (Date.today - (Date.today.wday) - 7)..(Date.today - (Date.today.wday) - 1)).sum(:expense)
-    @monthSum = Expense.where(date: Date.today.beginning_of_month..Date.today.end_of_month).sum(:expense)
-    @aggregate = Expense.where(date: Date.today.prev_month.beginning_of_month..Date.today.prev_month.end_of_month).sum(:expense)
+    @thisWeekSum =  Expense.where(user_id: current_user.id).where(date: (Date.today - (Date.today.wday))..Date.today).sum(:expense)
+    @lastWeekSum =  Expense.where(user_id: current_user.id).where(date: (Date.today - (Date.today.wday) - 7)..(Date.today - (Date.today.wday) - 1)).sum(:expense)
+    @monthSum = Expense.where(user_id: current_user.id).where(date: Date.today.beginning_of_month..Date.today.end_of_month).sum(:expense)
+    @aggregate = Expense.where(user_id: current_user.id).where(date: Date.today.prev_month.beginning_of_month..Date.today.prev_month.end_of_month).sum(:expense)
   end
 
 
